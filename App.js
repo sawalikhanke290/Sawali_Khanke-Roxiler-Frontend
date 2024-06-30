dd5ed6bdb0b3bbcc4adb6752b46d72f36ffa325d
SAWALI SANJAY KHANKE <102353162+sawalikhanke290@users.noreply.github.com>
2024-06-30 17:04:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Transactions from './Component//Transactions/transactions';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Transactions />
+    </div>
+  );
+}
+
+export default App;
